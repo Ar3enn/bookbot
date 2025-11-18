83e8706d4be55907ced3ed8759d7398d972f55f1
Ar3enn <ar3enn@gmail.com>
2025-11-18 22:13:49 +0300

    removed hardcoded arguments

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from stats import *
+import sys
     
 def main():
+    
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
     print("============ BOOKBOT ============")
-    text = get_book_text("./books/frankenstein.txt")
+    text = get_book_text(sys.argv[1])
     print("----------- Word Count ----------")
     print(f"Found {count_words(text)} total words")
     print("--------- Character Count -------")
     display_sorted(text)
     print("============= END ===============")
-
+   
+    
 
 main()    

138e098f51a721eb0aaa8562fc25fa9d08cc7749
Ar3enn <ar3enn@gmail.com>
2025-11-18 21:52:13 +0300

    added proper formatting

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from stats import *
     
 def main():
+    print("============ BOOKBOT ============")
     text = get_book_text("./books/frankenstein.txt")
+    print("----------- Word Count ----------")
     print(f"Found {count_words(text)} total words")
-    count_chars(text)
+    print("--------- Character Count -------")
+    display_sorted(text)
+    print("============= END ===============")
 
 
 main()    
--- stats.py
+++ stats.py
@@ -1,21 +1,32 @@
 def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
-        file_contents = f.read()
-        return file_contents
+        print(f"Analyzing book found at {filepath}")
+        return f.read()
 
-def count_words(file_contents: str):
-     return len(file_contents.split()) 
- 
-def count_chars(file_contents: str):
+def count_words(file_contents: str) -> int:
+    return len(file_contents.split()) 
+
+def count_chars(file_contents: str) -> list:
     chars = file_contents.lower()
     chars_dict = {}
+    
     for char in chars:
+        if not char.isalpha():
+            continue
         if char not in chars_dict:
             chars_dict[char] = 1
-        elif char in chars_dict:
-            chars_dict[char] += 1    
-        
-    print(chars_dict)
-        
-        
+        else:
+            chars_dict[char] += 1   
     
+    chars_list = [{"char": char, "num": count} for char, count in chars_dict.items()]
+    return chars_list
+
+def get_count(item):
+    return item["num"]
+
+def display_sorted(file_contents: str):
+    chars_list = count_chars(file_contents)
+    chars_list.sort(key=get_count, reverse=True)
+    for item in chars_list:
+        print(f"{item['char']} : {item['num']}")
+
